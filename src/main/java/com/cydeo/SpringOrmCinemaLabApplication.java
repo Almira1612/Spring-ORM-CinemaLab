16eb9d593865c2797c29361172f9e2fa023bc41e
Aimila Maimaitijiang <almira1612@github.com>
2022-10-26 22:57:12 -0400

    Created project

---
+++ src/main/java/com/cydeo/SpringOrmCinemaLabApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOrmCinemaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOrmCinemaLabApplication.class, args);
+    }
+
+}
