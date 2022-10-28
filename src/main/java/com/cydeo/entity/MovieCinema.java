f1fe7bbece0752ae0feb6761fbbe7411cfaee7cc
Aimila Maimaitijiang <almira1612@github.com>
2022-10-27 23:03:39 -0400

    Added Cinema and location entity classes

--- src/main/java/com/cydeo/entity/MovieCinema.java
+++ src/main/java/com/cydeo/entity/MovieCinema.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,4 +16,10 @@ public class MovieCinema extends BaseEntity{
 
     @Column(columnDefinition = "TIMESTAMP")
     private LocalDateTime dateTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Cinema cinema;
 }
