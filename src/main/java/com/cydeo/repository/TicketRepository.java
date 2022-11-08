ef1cb88597cff2d78fdabfeead9cc3796ac21406
Aimila Maimaitijiang <almira1612@gmail.com>
2022-11-07 23:19:44 -0500

    Adding new query to TicketRepository

--- src/main/java/com/cydeo/repository/TicketRepository.java
+++ src/main/java/com/cydeo/repository/TicketRepository.java
@@ -1,29 +1,46 @@
 package com.cydeo.repository;
 
+import com.cydeo.entity.Movie;
 import com.cydeo.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
     // ------------------- DERIVED QUERIES ------------------- //
 
     //Write a derived query to count how many tickets a user bought
+    Integer countAllByUserAccountId(Long userId);
 
     //Write a derived query to list all tickets by specific email
+    List<Ticket> findAllByUserAccountEmail(String email);
 
     //Write a derived query to count how many tickets are sold for a specific movie
+    Integer countAllByMovieCinemaMovieName(String name);
 
     //Write a derived query to list all tickets between a range of dates
+    List<Ticket> findAllByDateTimeBetween(LocalDateTime dateTime1, LocalDateTime dateTime2);
 
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query that returns all tickets are bought from a specific user
+    @Query("SELECT t FROM Ticket t WHERE t.userAccount.id = ?1")
+    List<Ticket> fetchAllTicketCountBySpecificUser(@Param("userId") Long UserId);
 
     //Write a JPQL query that returns all tickets between a range of dates
+    @Query("SELECT t FROM Ticket t WHERE t.dateTime BETWEEN ?1 AND ?2")
+    List<Ticket> fetchAllTicketsBetweenDateRange(@Param("dateTime1") LocalDateTime dateTime1, @Param("dateTime2") LocalDateTime dateTime2);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to count the number of tickets a user bought
+    @Query(value = "SELECT * ",nativeQuery = true)
+    Integer countAllByUserAccountId();
 
     //Write a native query to count the number of tickets a user bought in a specific range of dates
 
