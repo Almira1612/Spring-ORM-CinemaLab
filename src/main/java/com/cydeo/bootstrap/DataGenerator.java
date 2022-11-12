cc7af3eb64d164043318ea84c8cd44d7cba0cfe0
Aimila Maimaitijiang <almira1612@gmail.com>
2022-11-12 11:41:10 -0500

    Edit on dataGenerator class

--- src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -1,25 +1,29 @@
 package com.cydeo.bootstrap;
 
-import com.cydeo.entity.Account;
-import com.cydeo.repository.AccountRepository;
+import com.cydeo.repository.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component
 public class DataGenerator implements CommandLineRunner {
 
     private final AccountRepository accountRepository;
+    private final CinemaRepository cinemaRepository;
+    private final MovieCinemaRepository movieCinemaRepository;
+    private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
 
-    public DataGenerator(AccountRepository accountRepository) {
+    public DataGenerator(AccountRepository accountRepository, CinemaRepository cinemaRepository, MovieCinemaRepository movieCinemaRepository, TicketRepository ticketRepository, UserRepository userRepository) {
         this.accountRepository = accountRepository;
+        this.cinemaRepository = cinemaRepository;
+        this.movieCinemaRepository = movieCinemaRepository;
+        this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-
+      System.out.println(accountRepository.fetchAdminUsers() );
 
     }
 }
