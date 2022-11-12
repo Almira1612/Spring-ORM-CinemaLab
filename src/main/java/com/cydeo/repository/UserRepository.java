6491c34b0b48309d941e33aabbbc7bf8329c88c0
Aimila Maimaitijiang <almira1612@gmail.com>
2022-11-11 23:44:19 -0500

    Added native queries on UserRepository

--- src/main/java/com/cydeo/repository/UserRepository.java
+++ src/main/java/com/cydeo/repository/UserRepository.java
@@ -40,16 +40,24 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     //Write a JPQL query that returns all users?
     @Query("SELECT u FROM User u")
-    List<User> returnAllUsers();
+    List<User> returnAllUsersJPQL();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns all users that contain a specific name?
+    @Query(value = "SELECT * FROM user_account WHERE username CONTAINS ?1", nativeQuery = true)
+    List<User> returnAllUsersContainSpecificName(@Param("name") String name);
 
     //Write a native query that returns all users?
+    @Query(value = "SELECT * FROM user_account",nativeQuery = true)
+    List<User> returnAllUsersNative();
 
     //Write a native query that returns all users in the range of ages?
+    @Query(value = "SELECT * FROM account_details WHERE age BETWEEN ?1 AND ?2",nativeQuery = true)
+    List<User> returnAllUserInAgeRange(@Param("age1")Integer age1, @Param("age2")Integer age2);
 
     //Write a native query to read a user by email?
+    @Query(value = "SELECT * FROM user_account WHERE email = ?1", nativeQuery = true)
+    Optional<User> readByEmail(@Param("email")String email);
 
 }
